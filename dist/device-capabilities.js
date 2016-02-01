6073963c9a2e0b141e18fc5be212510720dbb6f9
Adi <adiinteractive@gmail.com>
2016-02-01 22:12:28 +0000

    added Speech

--- dist/device-capabilities.js
+++ dist/device-capabilities.js
@@ -1,4 +1,10 @@
 (function (console, $hx_exports) { "use strict";
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
 var BatterySupport = $hx_exports.BatterySupport = function(ready) {
 	var _g = this;
 	this._navigator = window.navigator;
@@ -135,8 +141,61 @@ Reflect.field = function(o,field) {
 	try {
 		return o[field];
 	} catch( e ) {
+		if (e instanceof js__$Boot_HaxeError) e = e.val;
 		return null;
 	}
+};
+var Speech = $hx_exports.Speech = function() {
+	if(Reflect.field(window,"SpeechRecognition") != null) this.recognition = Type.createInstance(Reflect.field(window,"SpeechRecognition"),[]); else if(Reflect.field(window,"webkitSpeechRecognition") != null) this.recognition = Type.createInstance(Reflect.field(window,"webkitSpeechRecognition"),[]);
+	if(Reflect.field(window,"SpeechGrammarList") != null) this.grammarList = Type.createInstance(Reflect.field(window,"SpeechGrammarList"),[]); else if(Reflect.field(window,"webkitSpeechGrammarList") != null) this.grammarList = Type.createInstance(Reflect.field(window,"webkitSpeechGrammarList"),[]);
+	this.isSupported = this.recognition != null && this.grammarList != null;
+};
+Speech.prototype = {
+	addGrammar: function(grammar) {
+		this.grammarList.addFromString(grammar,1);
+	}
+	,startRecognition: function(callback,endCallback) {
+		var _g = this;
+		this.recognition.grammars = this.grammarList;
+		this.recognition.lang = Speech.language;
+		this.recognition.interimResults = Speech.interimResults;
+		this.recognition.maxAlternatives = Speech.maxAlternatives;
+		this.recognition.start();
+		this.recognition.onresult = function(event) {
+			callback(event.results);
+		};
+		this.recognition.onspeechend = function() {
+			_g.recognition.stop();
+			if(endCallback != null) endCallback();
+		};
+	}
+};
+var Type = function() { };
+Type.createInstance = function(cl,args) {
+	var _g = args.length;
+	switch(_g) {
+	case 0:
+		return new cl();
+	case 1:
+		return new cl(args[0]);
+	case 2:
+		return new cl(args[0],args[1]);
+	case 3:
+		return new cl(args[0],args[1],args[2]);
+	case 4:
+		return new cl(args[0],args[1],args[2],args[3]);
+	case 5:
+		return new cl(args[0],args[1],args[2],args[3],args[4]);
+	case 6:
+		return new cl(args[0],args[1],args[2],args[3],args[4],args[5]);
+	case 7:
+		return new cl(args[0],args[1],args[2],args[3],args[4],args[5],args[6]);
+	case 8:
+		return new cl(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7]);
+	default:
+		throw new js__$Boot_HaxeError("Too many arguments");
+	}
+	return null;
 };
 var Vibration = $hx_exports.Vibration = function() {
 	this._navigator = window.navigator;
@@ -177,8 +236,20 @@ haxe_Timer.prototype = {
 	,run: function() {
 	}
 };
+var js__$Boot_HaxeError = function(val) {
+	Error.call(this);
+	this.val = val;
+	this.message = String(val);
+	if(Error.captureStackTrace) Error.captureStackTrace(this,js__$Boot_HaxeError);
+};
+js__$Boot_HaxeError.__super__ = Error;
+js__$Boot_HaxeError.prototype = $extend(Error.prototype,{
+});
 var $_, $fid = 0;
 function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+Speech.language = "en-GB";
+Speech.maxAlternatives = 1;
+Speech.interimResults = false;
 })(typeof console != "undefined" ? console : {log:function(){}}, typeof window != "undefined" ? window : exports);
 
 //# sourceMappingURL=device-capabilities.js.map
---
+++ src/Speech.hx
@@ -0,0 +1,52 @@
+import js.html.SpeechInputResultList;
+import js.Browser;
+import js.html.SpeechRecognitionEvent;
+import js.html.SpeechGrammarList;
+import js.html.SpeechRecognition;
+
+@:expose @:keep class Speech {
+
+    public static var language:String = "en-GB";
+    public static var maxAlternatives:Int = 1;
+    public static var interimResults:Bool = false;
+
+    /**
+	* Support check.
+	*
+	* @property isSupported
+	* @type {Bool}
+	*/
+    public var isSupported:Bool;
+
+    public var recognition:SpeechRecognition;
+    public var grammarList:SpeechGrammarList;
+
+    public function new() {
+        if (Reflect.field(Browser.window, "SpeechRecognition") != null) recognition = Type.createInstance(Reflect.field(Browser.window, "SpeechRecognition"), []);
+        else if (Reflect.field(Browser.window, "webkitSpeechRecognition") != null) recognition = Type.createInstance(Reflect.field(Browser.window, "webkitSpeechRecognition"), []);
+
+        if (Reflect.field(Browser.window, "SpeechGrammarList") != null) grammarList = Type.createInstance(Reflect.field(Browser.window, "SpeechGrammarList"), []);
+        else if (Reflect.field(Browser.window, "webkitSpeechGrammarList") != null) grammarList = Type.createInstance(Reflect.field(Browser.window, "webkitSpeechGrammarList"), []);
+
+        isSupported = (recognition != null && grammarList != null);
+    }
+
+    public function addGrammar(grammar:String) {
+        grammarList.addFromString(grammar, 1);
+    }
+
+    public function startRecognition(callback:Dynamic -> Void, ?endCallback:Void -> Void) {
+        recognition.grammars = grammarList;
+        recognition.lang = language;
+        recognition.interimResults = interimResults;
+        recognition.maxAlternatives = maxAlternatives;
+        recognition.start();
+        recognition.onresult = function(event:SpeechRecognitionEvent) {
+            callback(event.results);
+        };
+        recognition.onspeechend = function() {
+            recognition.stop();
+            if (endCallback != null) endCallback();
+        };
+    }
+}
